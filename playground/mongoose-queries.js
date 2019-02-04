a24d41f466c71f1ce925e5f05bc22b37d424966a
github username <elis.paul7@gmail.com>
2019-02-04 09:43:15 -0500

    Add queries file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,30 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// const id = '5c544c3b55989330d4f809df11';
+//
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid.');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo by Id', todo);
+// }).catch((err) => console.log(err));
